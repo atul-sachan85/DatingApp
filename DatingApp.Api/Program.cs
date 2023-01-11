441bc9f9410868c054767dc16874a0cfdb16c0e4
atul-sachan <34751474+atul-sachan@users.noreply.github.com>
2023-01-11 22:33:46 +0530

    Added Angular setup

--- DatingApp.Api/Program.cs
+++ DatingApp.Api/Program.cs
@@ -8,19 +8,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(option =>
 {
-  option.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+    option.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseCors(opt => opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-  app.UseSwagger();
-  app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 // app.UseHttpsRedirection();
